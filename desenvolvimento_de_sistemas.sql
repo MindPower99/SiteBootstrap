a3f22833e0e725d6d8d210296bfce7bcb820c067
MindPower99 <gabriel-jorge1999@hotmail.com>
2018-11-08 17:05:27 -0200

    Atualizando o Banco de Dados

--- desenvolvimento_de_sistemas.sql
+++ desenvolvimento_de_sistemas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 07-Nov-2018 às 01:26
+-- Generation Time: 08-Nov-2018 às 20:03
 -- Versão do servidor: 5.7.17
 -- PHP Version: 5.6.30
 
@@ -25,15 +25,38 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `banco de dados`
+--
+
+CREATE TABLE `banco de dados` (
+  `_Procedure` varchar(100) NOT NULL,
+  `_Trigger` varchar(100) NOT NULL,
+  `_Inserts` varchar(100) NOT NULL,
+  `_Update` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `duvidas`
 --
 
 CREATE TABLE `duvidas` (
-  `Nome` varchar(25) NOT NULL,
-  `Email` varchar(30) NOT NULL,
-  `Assunto` varchar(20) NOT NULL,
-  `Recado` varchar(200) NOT NULL
+  `Nome` varchar(20) NOT NULL,
+  `Email` varchar(35) NOT NULL,
+  `Assunto` varchar(30) NOT NULL,
+  `Recado` varchar(300) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `duvidas`
+--
+
+INSERT INTO `duvidas` (`Nome`, `Email`, `Assunto`, `Recado`) VALUES
+('Gabriel', 'gabriel-jorge1999@hotmail.com', '1', 'eaeawe'),
+('Gabriel', 'gabriel-jorge1999@hotmail.com', '1', 'esadasd'),
+('Gabriel', 'gabriel-jorge1999@hotmail.com', '1', 'eae mano de boa'),
+('Gabriel', 'gabriel-jorge1999@hotmail.com', 'Etec ZL', 'mano vai si fude');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
