ee1033247cf878360801550859b8e442237025fa
MindPower99 <gabriel-jorge1999@hotmail.com>
2018-11-13 16:23:15 -0200

    Separando o código javascript para esconder paragrafos em um unico arquivo

---
+++ assets/js/DuvidasEffects.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+									$(".pduvidas").hide();
+									$("#t1").click(MostrarParagrafo1);
+									$("#p1").click(EsconderParagrafo1);
+									$("#t2").click(MostrarParagrafo2);
+									$("#p2").click(EsconderParagrafo2);
+									$("#t3").click(MostrarParagrafo3);
+									$("#p3").click(EsconderParagrafo3);
+									$("#t4").click(MostrarParagrafo4);
+									$("#p4").click(EsconderParagrafo4);
+									});
+
+									function MostrarParagrafo1() {
+										$("#p1").show();
+									}
+									function EsconderParagrafo1() {
+										$("#p1").hide();
+									}
+									function MostrarParagrafo2() {
+										$("#p2").show();
+									}
+									function EsconderParagrafo2() {
+										$("#p2").hide();
+									}
+									function MostrarParagrafo3() {
+										$("#p3").show();
+									}
+									function EsconderParagrafo3() {
+										$("#p3").hide();
+									}
+									function MostrarParagrafo4() {
+										$("#p4").show();
+									}
+									function EsconderParagrafo4() {
+										$("#p4").hide();
+									}
