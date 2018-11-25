7928ceaf148ba7e85493aaf5c32e3f80e0bfe09a
MindPower99 <gabriel-jorge1999@hotmail.com>
2018-11-24 22:21:00 -0200

    Duvidas frequentes completa

--- assets/js/DuvidasEffects.js
+++ assets/js/DuvidasEffects.js
@@ -8,10 +8,12 @@ $(document).ready(function(){
 									$("#p3").click(EsconderParagrafo3);
 									$("#t4").click(MostrarParagrafo4);
 									$("#p4").click(EsconderParagrafo4);
-									$("#t4").click(MostrarParagrafo4);
-									$("#p4").click(EsconderParagrafo4);
-									$("#t4").click(MostrarParagrafo4);
-									$("#p4").click(EsconderParagrafo4);
+									$("#t5").click(MostrarParagrafo5);
+									$("#p5").click(EsconderParagrafo5);
+									$("#t6").click(MostrarParagrafo6);
+									$("#p6").click(EsconderParagrafo6);
+									$("#t7").click(MostrarParagrafo7);
+									$("#p7").click(EsconderParagrafo7);
 									});
 
 									function MostrarParagrafo1() {
@@ -37,4 +39,22 @@ $(document).ready(function(){
 									}
 									function EsconderParagrafo4() {
 										$("#p4").hide();
+									}
+									function MostrarParagrafo5() {
+										$("#p5").show();
+									}
+									function EsconderParagrafo5() {
+										$("#p5").hide();
+									}
+									function MostrarParagrafo6() {
+										$("#p6").show();
+									}
+									function EsconderParagrafo6() {
+										$("#p6").hide();
+									}
+									function MostrarParagrafo7() {
+										$("#p7").show();
+									}
+									function EsconderParagrafo7() {
+										$("#p7").hide();
 									}
